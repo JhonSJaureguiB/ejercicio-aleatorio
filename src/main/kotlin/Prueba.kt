c09199fcaa72e4a2913e5bb3c898a98aceb64b92
zip-b104-e-004 <julian.diaz-c@uniminuto.edu.co>
2024-08-16 15:31:00 -0500

    tercer commit

--- src/main/kotlin/Prueba.kt
+++ src/main/kotlin/Prueba.kt
@@ -1,3 +1,17 @@
+import kotlin.random.Random
+
 fun main(){
-    println("hola")
+    var moneda: Int
+    var cara: Int = 0
+    var sello: Int =0
+    for(i in 1..20){
+        moneda = Random.nextInt(2)
+        if (moneda == 0){
+            cara++
+        }
+        else{
+            sello++
+        }
+        println("$cara - $sello")
+    }
 }
